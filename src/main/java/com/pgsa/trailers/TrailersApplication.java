3537fa94a7b5ca5bdb7253e66ea5d1f1c81b068d
Zelesto <theozwane@gmail.com>
2026-02-04 09:35:15 +0200

    Add CommandLineRunner to TrailersApplication
    Implement CommandLineRunner to log active profile and JWT secret status.

--- src/main/java/com/pgsa/trailers/TrailersApplication.java
+++ src/main/java/com/pgsa/trailers/TrailersApplication.java
@@ -1,13 +1,31 @@
 package com.pgsa.trailers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrailersApplication {
+public class TrailersApplication implements CommandLineRunner {
+
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
+    @Value("${security.jwt.secret:NOT_SET}")
+    private String jwtSecret;
 
     public static void main(String[] args) {
         SpringApplication.run(TrailersApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("=== Active Spring Profile: " + activeProfile + " ===");
+
+        if ("NOT_SET".equals(jwtSecret)) {
+            System.err.println("!!! WARNING: JWT_SECRET is not set in environment variables !!!");
+        } else {
+            System.out.println("JWT secret is loaded successfully.");
+        }
+    }
 }

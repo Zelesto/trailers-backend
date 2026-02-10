df466f10bf4a92448e6553598e94c8c375dc541b
Zelesto <theozwane@gmail.com>
2026-02-10 15:01:31 +0200

    Update TripMetricsRepository.java

--- src/main/java/com/pgsa/trailers/repository/TripMetricsRepository.java
+++ src/main/java/com/pgsa/trailers/repository/TripMetricsRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TripMetricsRepository extends JpaRepository<TripMetrics, Long> {
-
     Optional<TripMetrics> findByTripId(Long tripId);
-
-
-
+    void deleteByTripId(Long tripId);
     boolean existsByTripId(Long tripId);
 }

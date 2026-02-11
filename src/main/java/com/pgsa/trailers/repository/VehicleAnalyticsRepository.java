90581acb0ac2f1253e558dcf188872f6f39bd6d8
Zelesto <theozwane@gmail.com>
2026-02-11 15:00:42 +0200

    Update VehicleAnalyticsRepository.java

--- src/main/java/com/pgsa/trailers/repository/VehicleAnalyticsRepository.java
+++ src/main/java/com/pgsa/trailers/repository/VehicleAnalyticsRepository.java
@@ -1,3 +1,13 @@
+package com.pgsa.trailers.repository;
+
+import com.pgsa.trailers.entity.assets.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface VehicleAnalyticsRepository extends JpaRepository<Vehicle, Long> {
 
     @Query(value = """

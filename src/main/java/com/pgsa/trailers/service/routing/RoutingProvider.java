3fe1a47b5e9cdf6b0c79b403c7ba7e2cd5f39812
Zelesto <theozwane@gmail.com>
2026-06-05 13:09:15 +0200

    Add RoutingProvider interface for routing services

--- src/main/java/com/pgsa/trailers/service/routing/RoutingProvider.java
+++ src/main/java/com/pgsa/trailers/service/routing/RoutingProvider.java
@@ -1,1 +1,12 @@
+package com.pgsa.trailers.service.routing;
 
+public interface RoutingProvider {
+
+    String name();
+
+    boolean supports(String vehicleType);
+
+    RoutingResult calculate(Coordinates origin,
+                            Coordinates destination,
+                            String vehicleType);
+}

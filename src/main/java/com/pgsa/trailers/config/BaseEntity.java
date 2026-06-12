3b4ffcbc0cb309cc94c8630c8aa60fe2d2291ec3
Zelesto <theozwane@gmail.com>
2026-06-12 11:46:56 +0200

    Fix import statement for AuditedEntity 1146

--- src/main/java/com/pgsa/trailers/config/BaseEntity.java
+++ src/main/java/com/pgsa/trailers/config/BaseEntity.java
@@ -1,6 +1,6 @@
 package com.pgsa.trailers.config;
 
-import com.pgsa.trailers.entity.security.AuditedEntity;
+import com.pgsa.trailers.entity.security;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;

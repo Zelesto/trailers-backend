d221a35040b95c6c89a077c5d010ba32ad0b3600
Zelesto <theozwane@gmail.com>
2026-06-12 21:01:09 +0200

    Add BigDecimal import to TripRepository 2101

--- src/main/java/com/pgsa/trailers/repository/TripRepository.java
+++ src/main/java/com/pgsa/trailers/repository/TripRepository.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.math.BigDecimal;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {

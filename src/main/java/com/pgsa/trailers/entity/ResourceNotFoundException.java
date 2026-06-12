effc9d22a002fe223b309e475d38cc74489d0df4
Zelesto <theozwane@gmail.com>
2026-06-12 11:28:07 +0200

    Refactor ResourceNotFoundException for better usage 1128

--- src/main/java/com/pgsa/trailers/entity/ResourceNotFoundException.java
+++ src/main/java/com/pgsa/trailers/entity/ResourceNotFoundException.java
@@ -1,7 +1,14 @@
-package com.pgsa.trailers.entity;
+package com.pgsa.trailers.exception;
 
+import lombok.Getter;
 
+@Getter
 public class ResourceNotFoundException extends RuntimeException {
+
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
     public ResourceNotFoundException() {
         super();
     }
@@ -13,7 +20,38 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-    super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-}
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public ResourceNotFoundException(Class<?> resourceClass, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceClass.getSimpleName(), fieldName, fieldValue));
+        this.resourceName = resourceClass.getSimpleName();
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public static ResourceNotFoundException forTrip(Long tripId) {
+        return new ResourceNotFoundException("Trip", "id", tripId);
+    }
+
+    public static ResourceNotFoundException forDriver(Long driverId) {
+        return new ResourceNotFoundException("Driver", "id", driverId);
+    }
+
+    public static ResourceNotFoundException forVehicle(Long vehicleId) {
+        return new ResourceNotFoundException("Vehicle", "id", vehicleId);
+    }
+
+    public static ResourceNotFoundException forLoad(Long loadId) {
+        return new ResourceNotFoundException("Load", "id", loadId);
+    }
+
+    public static ResourceNotFoundException forTripNumber(String tripNumber) {
+        return new ResourceNotFoundException("Trip", "tripNumber", tripNumber);
+    }
 }

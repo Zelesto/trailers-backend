4e11d165f7790e83d02b33ccfbf16c8cd512f29e
Zelesto <theozwane@gmail.com>
2026-06-12 11:29:53 +0200

    Add TripValidationException for trip validation errors 1129

---
+++ src/main/java/com/pgsa/trailers/entity/suppliers/TripValidationException .java
@@ -0,0 +1,53 @@
+package com.pgsa.trailers.exception;
+
+import lombok.Getter;
+
+@Getter
+public class TripValidationException extends RuntimeException {
+
+    private String errorCode;
+    private Object[] args;
+
+    public TripValidationException(String message) {
+        super(message);
+    }
+
+    public TripValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TripValidationException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public TripValidationException(String message, String errorCode, Object... args) {
+        super(message);
+        this.errorCode = errorCode;
+        this.args = args;
+    }
+
+    public static TripValidationException invalidStatusTransition(String from, String to) {
+        return new TripValidationException(
+            String.format("Invalid status transition from %s to %s", from, to),
+            "INVALID_STATUS_TRANSITION",
+            from, to
+        );
+    }
+
+    public static TripValidationException missingRequiredField(String fieldName) {
+        return new TripValidationException(
+            String.format("Required field missing: %s", fieldName),
+            "MISSING_REQUIRED_FIELD",
+            fieldName
+        );
+    }
+
+    public static TripValidationException invalidOdometerReading(BigDecimal start, BigDecimal end) {
+        return new TripValidationException(
+            String.format("Invalid odometer reading: start=%.2f, end=%.2f", start, end),
+            "INVALID_ODOMETER_READING",
+            start, end
+        );
+    }
+}

8374e9ed1d4fc3e4e577ceb64587a2b66435d8a8
Zelesto <theozwane@gmail.com>
2026-06-15 10:26:16 +0200

    Add constructors to TripResponse using Lombok 1026

--- src/main/java/com/pgsa/trailers/dto/TripResponse.java
+++ src/main/java/com/pgsa/trailers/dto/TripResponse.java
@@ -1,14 +1,18 @@
 package com.pgsa.trailers.dto;
 
 import com.pgsa.trailers.enums.TripStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TripResponse {
 
     private Long id;

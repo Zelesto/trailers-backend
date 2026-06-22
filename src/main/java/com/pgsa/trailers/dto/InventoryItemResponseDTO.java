b3cdfc1388b13eddac8e7b06d9d378b6e9579825
Zelesto <theozwane@gmail.com>
2026-06-22 13:23:55 +0200

    Create InventoryItemResponseDTO class 1323

---
+++ src/main/java/com/pgsa/trailers/dto/InventoryItemResponseDTO.java
@@ -0,0 +1,24 @@
+// src/main/java/com/pgsa/trailers/dto/inventory/InventoryItemResponseDTO.java
+package com.pgsa.trailers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryItemResponseDTO {
+    private Long id;
+    private String name;
+    private String category;
+    private String unitOfMeasure;
+    private Boolean isConsumable;
+    private BigDecimal reorderLevel;
+    private Long locationId;
+    private String locationName;
+}

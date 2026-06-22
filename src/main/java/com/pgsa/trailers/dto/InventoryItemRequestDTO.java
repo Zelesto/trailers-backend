e191dc5a9bce94a7a66fa855719945db5f7d87ab
Zelesto <theozwane@gmail.com>
2026-06-22 13:23:23 +0200

    Create InventoryItemRequestDTO class for inventory items 1323

---
+++ src/main/java/com/pgsa/trailers/dto/InventoryItemRequestDTO.java
@@ -0,0 +1,22 @@
+// src/main/java/com/pgsa/trailers/dto/inventory/InventoryItemRequestDTO.java
+package com.pgsa.trailers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryItemRequestDTO {
+    private String name;
+    private String category;
+    private String unitOfMeasure;
+    private Boolean isConsumable;
+    private BigDecimal reorderLevel;
+    private Long locationId;
+}

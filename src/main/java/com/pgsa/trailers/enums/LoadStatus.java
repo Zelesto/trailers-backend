84436748c5db78f081c58f8a8f25a8e4b8392204
Zelesto <theozwane@gmail.com>
2026-06-24 13:09:03 +0200

    Add LoadStatus enum for trailer statuses 1309

---
+++ src/main/java/com/pgsa/trailers/enums/LoadStatus.java
@@ -0,0 +1,11 @@
+// src/main/java/com/pgsa/trailers/enums/LoadStatus.java
+package com.pgsa.trailers.enums;
+
+public enum LoadStatus {
+    PENDING,
+    LOADED,
+    IN_TRANSIT,
+    DELIVERED,
+    COMPLETED,
+    CANCELLED
+}

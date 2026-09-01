f702af530ce9c5d0729a330d33c06009758bf8a0
Zelesto <theozwane@gmail.com>
2026-09-01 16:10:44 +0200

    Add comment to InvoiceRepository for clarity

--- src/main/java/com/pgsa/trailers/repository/finance/InvoiceRepository.java
+++ src/main/java/com/pgsa/trailers/repository/finance/InvoiceRepository.java
@@ -1,5 +1,6 @@
 package com.pgsa.trailers.repository.finance;
 
+// USE finance.Invoice
 import com.pgsa.trailers.entity.finance.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;

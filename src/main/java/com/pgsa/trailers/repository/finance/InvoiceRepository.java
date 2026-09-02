7a2db981116a701f33f6ddb193dcdf06e67a50d9
Zelesto <theozwane@gmail.com>
2026-09-02 10:53:41 +0200

    Remove unused comment in InvoiceRepository

--- src/main/java/com/pgsa/trailers/repository/finance/InvoiceRepository.java
+++ src/main/java/com/pgsa/trailers/repository/finance/InvoiceRepository.java
@@ -1,6 +1,5 @@
 package com.pgsa.trailers.repository.finance;
 
-// USE finance.Invoice
 import com.pgsa.trailers.entity.finance.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;

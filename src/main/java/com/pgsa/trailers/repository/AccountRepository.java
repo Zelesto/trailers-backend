e3b0f0ed5d1ce9cf8bb58255f0bd60a3234acfc8
Zelesto <theozwane@gmail.com>
2026-09-02 10:51:22 +0200

    Add findByAccountNumber method to AccountRepository
    Reintroduced findByAccountNumber method to AccountRepository.

--- src/main/java/com/pgsa/trailers/repository/AccountRepository.java
+++ src/main/java/com/pgsa/trailers/repository/AccountRepository.java
@@ -1,26 +1,22 @@
 package com.pgsa.trailers.repository;
 
 import com.pgsa.trailers.entity.finance.Account;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    // Use AccountType enum
     List<Account> findByType(String type);
 
-    // Find active accounts
     List<Account> findByActiveTrue();
 
-    // Find by name containing (case-insensitive)
     List<Account> findByNameContainingIgnoreCase(String name);
 
-    // Find by provider
     List<Account> findByProvider(String provider);
 
-    // REMOVE or COMMENT OUT this line if Account entity doesn't have accountNumber:
-    // Optional<Account> findByAccountNumber(String accountNumber);
+    Optional<Account> findByAccountNumber(String accountNumber);
 }

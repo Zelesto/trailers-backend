e1e162c0bb85eaa89ce6cdc170033e3d1db5120c
Zelesto <theozwane@gmail.com>
2026-09-02 13:24:04 +0200

    Add Logger import to LeaveController

--- src/main/java/com/pgsa/trailers/controller/LeaveController.java
+++ src/main/java/com/pgsa/trailers/controller/LeaveController.java
@@ -8,6 +8,8 @@ import com.pgsa.trailers.service.LeaveService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
